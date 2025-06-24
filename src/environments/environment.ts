9a30357788cba8f4361abef07d6cdc1d73c0b2e5
PacebDevs <pacebb@gmail.com>
2025-06-24 20:43:42 +0200

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,13 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.131:3000/api',
-  //apiUrl: 'http://10.0.2.2:3000/api'
-  imageURL: 'http://192.168.1.131:3000/'
+  //Piso y casa de padres
+ // apiUrl: 'http://192.168.1.131:3000/api',
+ // imageURL: 'http://192.168.1.131:3000/'
+//Parcela
+   apiUrl: 'http://192.168.1.242:3000/api',
+  imageURL: 'http://192.168.1.242:3000/'
+
 
 };
 
